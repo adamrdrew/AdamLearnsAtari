f413786bdca8e0fc0223a23d52eab66852e1f539
Adam Drew <adamrdrew@live.com>
2021-11-22 14:51:29 -0500

    Sprite stuff. Flipping. Animation. Pointers

---
+++ face.asm
@@ -0,0 +1,12 @@
+;Created using Atari Dev Studio
+;assembly format (bottom to top)
+
+Sprite1:
+	.byte %01111110 ;
+	.byte %11000011 ;
+	.byte %10111101 ;
+	.byte %11111111 ;
+	.byte %10011001 ;
+	.byte %10011001 ;
+	.byte %11111111 ;
+	.byte %01111110 ;
